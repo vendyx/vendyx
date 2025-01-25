61aeddbba88a7b06e1083ea061a4e614acb3e136
Brayan Carreño <andrebrayan74@gmail.com>
2025-01-25 16:30:54 -0500

    chore(admin): remove redirect on collections

--- apps/vendyx-admin/src/core/collection/actions/remove-massive-collection.ts
+++ apps/vendyx-admin/src/core/collection/actions/remove-massive-collection.ts
@@ -1,12 +1,9 @@
 'use server';
 
 import { revalidateTag } from 'next/cache';
-import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 import { type ID } from '@/api/scalars/scalars.type';
 import { CollectionService } from '@/api/services/collection.service';
-import { getBasePathFormHeaders } from '@/shared/utils/url';
 
 export const removeMassiveCollection = async (ids: ID[]) => {
   const isOk = await CollectionService.remove(ids);
@@ -15,8 +12,5 @@ export const removeMassiveCollection = async (ids: ID[]) => {
     return { error: 'Failed to remove products' };
   }
 
-  const base = getBasePathFormHeaders(headers());
-
   revalidateTag(CollectionService.Tags.collections);
-  redirect(`${base}/collections`);
 };
